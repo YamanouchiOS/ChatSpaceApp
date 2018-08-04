9b6f4632d32f0afc5a3a90370df2b0491d252bea
YamanouchiOS <tomoaki.yamanouchi@gmail.com>
2018-08-05 00:05:29 +0900

    aws

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,8 @@
 lock '3.11.0'
 
 set :application, 'ChatSpaceApp'
-set :repo_url,  'git@github.com/YamanouchiOS/ChatSpaceApp.git'
+set :repo_url,  'git@github.com:YamanouchiOS/ChatSpaceApp.git'
+
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
