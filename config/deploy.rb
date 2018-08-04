2521a642bf3cd7a3b872c648af3094c439828fcc
YamanouchiOS <tomoaki.yamanouchi@gmail.com>
2018-08-04 22:44:11 +0900

    Was

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.11.0'
 
 set :application, 'ChatSpaceApp'
 set :repo_url,  'git@github.com/YamanouchiOS/ChatSpaceApp.git'
